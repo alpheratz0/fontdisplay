d111f4b8279f78189a14b5f16c881b0f865c2f9f
alpheratz0 <alpheratz99@protonmail.com>
2022-06-21 14:11:33 -0300

    refactor: major refactor of `bitmap.c`

--- src/base/bitmap.c
+++ src/base/bitmap.c
@@ -16,63 +16,73 @@
 */
 
 #include <stdlib.h>
+#include <stdint.h>
 
-#include "../util/numdef.h"
 #include "../util/debug.h"
 #include "bitmap.h"
 
 extern bitmap_t *
-bitmap_create(u32 width, u32 height, u32 color)
+bitmap_create(uint32_t width, uint32_t height, uint32_t color)
 {
 	bitmap_t *bmp;
 
-	if ((bmp = malloc(sizeof(bitmap_t)))) {
-		bmp->width = width;
-		bmp->height = height;
-		if ((bmp->px = malloc(4*width*height))) {
-			for (u32 i = 0; i < width * height; ++i) {
-				bmp->px[i] = color;
-			}
-			return bmp;
-		}
+	if (NULL == (bmp = malloc(sizeof(bitmap_t)))) {
+		die("error while calling malloc, no memory available");
 	}
 
-	die("error while calling malloc, no memory available");
+	bmp->width = width;
+	bmp->height = height;
 
-	return (void *)(0);
+	if (NULL == (bmp->px = malloc(sizeof(uint32_t) * width * height))) {
+		die("error while calling malloc, no memory available");
+	}
+
+	bitmap_clear(bmp, color);
+
+	return bmp;
 }
 
 extern void
-bitmap_set(bitmap_t *bmp, u32 x, u32 y, u32 color)
+bitmap_set(bitmap_t *bmp, uint32_t x, uint32_t y, uint32_t color)
 {
 	if (x < bmp->width && y < bmp->height) {
 		bmp->px[y*bmp->width+x] = color;
 	}
 }
 
-extern u32
-bitmap_get(bitmap_t *bmp, u32 x, u32 y)
+extern uint32_t
+bitmap_get(bitmap_t *bmp, uint32_t x, uint32_t y)
 {
 	if (x < bmp->width && y < bmp->height) {
 		return bmp->px[y*bmp->width+x];
 	}
+
 	return 0;
 }
 
 extern void
-bitmap_rect(bitmap_t *bmp, u32 x, u32 y, u32 width, u32 height, u32 color)
+bitmap_rect(bitmap_t *bmp,
+            uint32_t x,
+            uint32_t y,
+            uint32_t width,
+            uint32_t height,
+            uint32_t color)
 {
-	for (u32 i = 0; i < width; ++i) {
-		for (u32 j = 0; j < height; ++j) {
-			bitmap_set(bmp, x + i, y + j, color);
+	for (uint32_t i = 0; i < width; ++i) {
+		for (uint32_t j = 0; j < height; ++j) {
+			bmp->px[(j+y)*bmp->width+i+x] = color;
 		}
 	}
 }
 
 extern void
-bitmap_clear(bitmap_t *bmp, u32 color)
+bitmap_clear(bitmap_t *bmp, uint32_t color)
 {
-	bitmap_rect(bmp, 0, 0, bmp->width, bmp->height, color);
+	for (uint32_t x = 0; x < bmp->width; ++x) {
+		for (uint32_t y = 0; y < bmp->height; ++y) {
+			bmp->px[y*bmp->width+x] = color;
+		}
+	}
 }
 
 extern void
