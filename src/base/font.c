ceb5141d3932783a37296ea574ff078797c77960
alpheratz0 <alpheratz99@protonmail.com>
2022-06-21 14:11:42 -0300

    refactor: major refactor of `font.c`

--- src/base/font.c
+++ src/base/font.c
@@ -16,14 +16,22 @@
 */
 
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <ft2build.h>
 #include FT_FREETYPE_H
 #include <fontconfig/fontconfig.h>
 
 #include "../util/debug.h"
-#include "../util/numdef.h"
 #include "font.h"
+
+#define CHKFTERR(name,err) do {                                 \
+	FT_Error error;                                             \
+	error = (err);                                              \
+	if (error != 0) {                                           \
+		dief(name " failed with error code: %d", (int)(error)); \
+	}                                                           \
+} while (0)
 
 static char *
 font_search(const char *family)
@@ -31,6 +39,7 @@ font_search(const char *family)
 	FcPattern *pattern;
 	FcPattern *match;
 	FcResult result;
+	FcValue v;
 	char *path = NULL;
 
 	if ((pattern = FcNameParse((const FcChar8 *)(family)))) {
@@ -38,8 +47,6 @@ font_search(const char *family)
 		FcDefaultSubstitute(pattern);
 
 		if ((match = FcFontMatch(0, pattern, &result))) {
-			FcValue v;
-
 			FcPatternGet(match, FC_FAMILY, 0, &v);
 
 			if (strcmp(family, (char *)(v.u.s)) == 0) {
@@ -58,42 +65,32 @@ font_search(const char *family)
 }
 
 extern font_t *
-font_load(const char *family, u32 size)
+font_load(const char *family, uint32_t size)
 {
 	char *path;
+	FT_Library lib;
+	FT_Face face;
 	font_t *font;
 
-	if (!(path = font_search(family))) {
+	if (NULL == (path = font_search(family))) {
 		dief("font family not found: %s", family);
 	}
 
-	if (!(font = malloc(sizeof(font_t)))) {
+	CHKFTERR("FT_Init_FreeType", FT_Init_FreeType(&lib));
+	CHKFTERR("FT_New_Face", FT_New_Face(lib, path, 0, &face));
+	CHKFTERR("FT_Set_Char_Size", FT_Set_Char_Size(face, 0, size * 64, 72, 72));
+	CHKFTERR("FT_Load_Glyph", FT_Load_Glyph(face, FT_Get_Char_Index(face, '0'), 0));
+	CHKFTERR("FT_Render_Glyph", FT_Render_Glyph(face->glyph, FT_RENDER_MODE_NORMAL));
+
+	if (NULL == (font = malloc(sizeof(font_t)))) {
 		die("error while calling malloc, no memory available");
 	}
 
-	if (FT_Init_FreeType(&font->library)) {
-		die("error while calling FT_Init_FreeType()");
-	}
-
-	if (FT_New_Face(font->library, path, 0, &(font->face))) {
-		die("error while calling FT_New_Face()");
-	}
-
-	if (FT_Set_Char_Size(font->face, 0, size * 64, 72, 72)) {
-		die("error while calling FT_Set_Char_Size()");
-	}
-
-	if (FT_Load_Glyph(font->face, FT_Get_Char_Index(font->face, '0'), 0)) {
-		die("error while calling FT_Load_Glyph()");
-	}
-
-	if (FT_Render_Glyph(font->face->glyph, FT_RENDER_MODE_NORMAL)) {
-		die("error while calling FT_Render_Glyph()");
-	}
-
+	font->library = lib;
+	font->face = face;
 	font->size = size;
-	font->width = font->face->glyph->advance.x >> 6;
-	font->height = (font->face->size->metrics.ascender - font->face->size->metrics.descender) >> 6;
+	font->width = face->glyph->advance.x >> 6;
+	font->height = (face->size->metrics.ascender - face->size->metrics.descender) >> 6;
 
 	free(path);
 
@@ -103,13 +100,8 @@ font_load(const char *family, u32 size)
 extern FT_GlyphSlot
 font_get_glyph(font_t *font, char c)
 {
-	if (FT_Load_Glyph(font->face, FT_Get_Char_Index(font->face, c), 0)) {
-		die("error while calling FT_Load_Glyph()");
-	}
-
-	if (FT_Render_Glyph(font->face->glyph, FT_RENDER_MODE_NORMAL)) {
-		die("error while calling FT_Render_Glyph()");
-	}
+	CHKFTERR("FT_Load_Glyph", FT_Load_Glyph(font->face, FT_Get_Char_Index(font->face, c), 0));
+	CHKFTERR("FT_Render_Glyph", FT_Render_Glyph(font->face->glyph, FT_RENDER_MODE_NORMAL));
 
 	return font->face->glyph;
 }
