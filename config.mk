b900fff2f638744d214a5297e8250c1a394895ef
alpheratz0 <alpheratz99@protonmail.com>
2022-10-11 13:55:43 -0300

    fix openbsd build

--- config.mk
+++ config.mk
@@ -4,10 +4,10 @@
 VERSION   = 1.0.1
 
 CC        = cc
-INCS      = -I/usr/include/freetype2
+INCS      = -I/usr/include/freetype2 -I/usr/X11R6/include
 CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
 LDLIBS    = -lxcb -lfreetype -lxcb-image -lfontconfig
-LDFLAGS   = -s
+LDFLAGS   = -L/usr/X11R6/lib -s
 
 PREFIX    = /usr/local
 MANPREFIX = $(PREFIX)/share/man
