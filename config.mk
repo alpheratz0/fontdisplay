353589071827b10c2d218e5717318d2f291ed1cd
alpheratz0 <alpheratz99@protonmail.com>
2023-08-22 22:49:56 -0300

    format config.mk

--- config.mk
+++ config.mk
@@ -1,13 +1,11 @@
 # Copyright (C) 2022 <alpheratz99@protonmail.com>
 # This program is free software.
 
-VERSION   = 1.0.2
-
-CC        = cc
-INCS      = -I/usr/include/freetype2 -I/usr/X11R6/include
-CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
-LDLIBS    = -lxcb -lfreetype -lxcb-image -lfontconfig
-LDFLAGS   = -L/usr/X11R6/lib -s
-
-PREFIX    = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+VERSION=1.0.2
+CC=cc
+INCS=-I/usr/include/freetype2 -I/usr/X11R6/include
+CFLAGS=-std=c99 -pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
+LDLIBS=-lxcb -lfreetype -lxcb-image -lfontconfig
+LDFLAGS=-L/usr/X11R6/lib -s
+PREFIX=/usr/local
+MANPREFIX=$(PREFIX)/share/man
