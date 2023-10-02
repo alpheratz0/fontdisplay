2acf69efea1c62e09332f5b459af1294cddab9a6
alpheratz0 <alpheratz99@protonmail.com>
2023-10-02 17:22:41 -0300

    update config.mk

--- config.mk
+++ config.mk
@@ -2,12 +2,18 @@
 # This program is free software.
 
 VERSION = 2.0.0
+
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
+
 PKG_CONFIG = pkg-config
+
 DEPENDENCIES = fcft xcb xcb-shm xcb-image pixman-1
+
 INCS = $(shell $(PKG_CONFIG) --cflags $(DEPENDENCIES)) -Iinclude
 LIBS = $(shell $(PKG_CONFIG) --libs $(DEPENDENCIES))
-CFLAGS = -Os $(INCS) $(CPPFLAGS) -DVERSION=\"$(VERSION)\"
+
+CFLAGS = -Os $(INCS) -DVERSION=\"$(VERSION)\"
 LDFLAGS = -s $(LIBS)
+
 CC = cc
